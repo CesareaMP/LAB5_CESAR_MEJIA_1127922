894090013cb535cca2f428ed731ba318a3b5083e
CESAR ALESSANDRO MEJIA PEREZ <camejiap@correo.url.edu.gt>
2022-10-09 16:29:27 -0600

    agregar list.h a proyecto

---
+++ LAB5_CESAR_MEJIA_1127922/List.h
@@ -0,0 +1,24 @@
+#pragma once
+class List
+{
+	struct Node
+	{
+		int data;
+		Node* next;
+	};
+private:
+	Node* header = nullptr;
+public:
+	void Add(int item);
+	void Clear();
+	int Count();
+	bool Contains(int item);
+	int IndexOf(int item);
+	void Insert(int index, int item);
+	int GetItem(int index);
+	void SetItem(int index, int item);
+	int LastIndexOf(int item);
+	bool Remove(int item);
+	void RemoveAt(int index);
+};
+
